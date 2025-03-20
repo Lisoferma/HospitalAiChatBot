1323cd1fe610c0476764c5f88bdeed614fb33400
nemonology <nemonology@mail.ru>
2025-03-21 00:56:07 +0900

    Added

---
+++ Source/Models/IAsynchrousChatClient.cs
@@ -0,0 +1,12 @@
+using System.Net;
+
+namespace HospitalAIChatbot.Source.Models
+{
+    public interface IAsynchrousChatClient : IDisposable
+    {
+        Task ConnectAsync(IPEndPoint ipEndPoint, CancellationToken cancellationToken = default);
+        Task DisconnectAsync(CancellationToken cancellationToken = default);
+        Task<string> ReceiveMessageAsync(CancellationToken cancellationToken = default);
+        Task SendMessageAsync(string message, CancellationToken cancellationToken = default);
+    }
+}
