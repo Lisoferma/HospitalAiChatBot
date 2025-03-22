fd8abb54e5051d1e6799b2126b9e40e3cde9d8b2
nemonology <nemonology@mail.ru>
2025-03-22 17:23:11 +0900

    Added ollama chat configuration struct

---
+++ Source/Models/OllamaApiConfiguration.cs
@@ -0,0 +1,18 @@
+namespace HospitalAIChatbot.Source.Models
+{
+    public struct OllamaChatClientConfiguration
+    {
+        public Uri ApiUri;
+        public string ModelName;
+        public string? Suffix = null;
+        public bool IsStreamResponce = true;
+
+        public OllamaChatClientConfiguration(Uri apiUri, string modelName, string? suffix = null, bool isStreamResponce = true)
+        {
+            ApiUri = apiUri;
+            ModelName = modelName;
+            Suffix = suffix;
+            IsStreamResponce = isStreamResponce;
+        }
+    }
+}
