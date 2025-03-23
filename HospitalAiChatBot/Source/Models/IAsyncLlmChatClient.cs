72a5015084e7e72c9178637b6dd01ccf7b4c6005
nemonology <nemonology@mail.ru>
2025-03-23 19:00:24 +0900

    Added LLM chat client interface

---
+++ HospitalAiChatBot/Source/Models/IAsyncLlmChatClient.cs
@@ -0,0 +1,12 @@
+namespace HospitalAiChatBot.Source.Models
+{
+    public interface IAsyncLlmChatClient : IDisposable
+    {
+        /// <summary>
+        /// Отправляет сообщение для LLM
+        /// </summary>
+        /// <returns>Ответ от LLM</returns>
+        /// <exception cref="ObjectDisposedException"/>
+        Task<string?> SendPromtAsync(string promt, CancellationToken cancellationToken = default);
+    }
+}
