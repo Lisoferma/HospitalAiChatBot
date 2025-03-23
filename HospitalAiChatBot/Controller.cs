04f18ff1b1f007b9f675b53cb813c7b43446c433
aril114 <aeaw7744@gmail.com>
2025-03-23 13:41:19 +0900

    Рабочая, но сырая версия контроллера Существенно упрощен Program.cs. Все, что в нем находилось, по большей части вынесено в отдельный файл Controller.cs Теперь приложение предлагает выбрать с помощью кнопок действие, которое желает совершить пользователь. Но вместо настоящего выполнения сценариев пользователь получает захардкоженные строки в ответ на "часы" и "контакты"

---
+++ HospitalAiChatBot/Controller.cs
@@ -0,0 +1,120 @@
+﻿using HospitalAIChatbot.Source.Models;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Controller
+{
+    private TelegramBotClient bot;
+    private Dictionary<long, UserState> idToUserState;
+    //private IHospitalSiteScraper scraper;
+
+    public Controller(TelegramBotClient aBot, Dictionary<long, UserState> idToUserState)
+    {
+        bot = aBot;
+
+        bot.OnError += OnError;
+        bot.OnMessage += OnMessage;
+
+        this.idToUserState = idToUserState;
+
+        //scraper = new RzdMedicineSiteScraper();
+    }
+
+    async Task OnStartMessage(Message msg)
+    {
+        long userId = msg.From.Id;
+
+        string startMsg;
+
+        if (idToUserState.ContainsKey(userId))
+        {
+            UserState state = idToUserState[userId];
+            state.IsAtStart = true;
+            startMsg = "Что-нибудь еще?";
+        }
+
+        else
+        {
+            startMsg = """
+            Здравствуйте!
+            Я могу записать вас к врачу, поделиться с вами информацией о поликлинике и т.п.
+            Что вы хотите сделать?
+            """;
+            UserState state = new UserState();
+            idToUserState.Add(userId, state);
+        }
+
+
+        Dictionary<Scenarios, String> scenarioToStr = new();
+
+        scenarioToStr.Add(Scenarios.GetWorkTimeAndContacts, "Контакты колл-центра и часы работы");
+        scenarioToStr.Add(Scenarios.GetDoctorWorkTime, "Часы работы специалиста");
+        scenarioToStr.Add(Scenarios.GetExaminationPrepareInfo, "Подготовка к исследованию");
+        scenarioToStr.Add(Scenarios.GetSamplesPreparingTimeInfo, "Время изготовления анализов");
+        scenarioToStr.Add(Scenarios.MakeAppointment, "Записаться к специалисту");
+        scenarioToStr.Add(Scenarios.Feedback, "Обратная связь");
+        scenarioToStr.Add(Scenarios.CommunicationWithOperator, "Связаться с оператором");
+
+        string[] buttonNames = scenarioToStr.Values.ToArray();
+
+        string[][] customKeyboard = new string[scenarioToStr.Count][];
+
+        for (int i = 0; i < buttonNames.Length; i++)
+        {
+            customKeyboard[i] = new string[] { buttonNames[i] };
+        }
+
+        await bot.SendMessage(msg.Chat, startMsg, replyMarkup: customKeyboard);
+    }
+
+
+
+    // Общие методы: обработчик ошибок и обработчик получений сообщений
+
+    // Обработки разных ошибок
+    async Task OnError(Exception exception, HandleErrorSource source)
+    {
+        Console.WriteLine(exception);
+    }
+
+    // Обработка полученных сообщений
+    async Task OnMessage(Message msg, UpdateType type)
+    {
+        // Если в таблице не хранится состояния пользователя, тоже переходим к OnStartMessage
+        if (msg.Text == "/start" || !idToUserState.ContainsKey(msg.From.Id))
+        {
+            await OnStartMessage(msg);
+        }
+
+        else if (msg.Text.ToLower().Contains("часы"))
+        {
+            string workingHours = "Пн-пт: с 08:00 до 20:00\nСб: с 08:00 до 14:00";
+            await bot.SendMessage(msg.Chat, workingHours);
+            await OnStartMessage(msg);
+        }
+
+        else if (msg.Text.ToLower().Contains("контакты"))
+        {
+            string contacts = "Баргузинская 49";
+            await bot.SendMessage(msg.Chat, contacts);
+            await OnStartMessage(msg);
+        }
+    }
+}
+
+/// <summary>
+/// Состояние пользователя
+/// </summary>
+class UserState
+{
+    public Scenarios CurrentScenario { get; set; }
+    public List<string>? ImportantMessages { get; }
+    public bool IsAtStart { get; set; }
+
+    public UserState()
+    {
+        IsAtStart = true;
+    }
+}
--- HospitalAiChatBot/Program.cs
+++ HospitalAiChatBot/Program.cs
@@ -1,14 +1,23 @@
-﻿using HospitalAIChatbot.Source.Models;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Telegram.Bot;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
 const string TOKEN_FILENAME = "token.txt";
 const string USERSTATE_FILENAME = "userstate.json";
 
-string BOT_TOKEN = File.ReadAllText(TOKEN_FILENAME);
+string botToken;
+
+try
+{
+    botToken = File.ReadAllText(TOKEN_FILENAME);
+}
+
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Не найден файл с токеном token.txt");
+    Console.WriteLine("Программа завершается");
+    return;
+}
+
 
 Dictionary<long, UserState> idToUserState;
 
@@ -21,77 +30,24 @@ try
 
 catch (FileNotFoundException)
 {
+    Console.WriteLine("Файл с состояниями пользователей не найден");
+    Console.WriteLine("Состояния пользователей обнуляются");
     idToUserState = new Dictionary<long, UserState>();
 }
 
 
 using var cts = new CancellationTokenSource();
-var bot = new TelegramBotClient(BOT_TOKEN);
-
-bot.OnError += OnError;
-bot.OnMessage += OnMessage;
-bot.OnUpdate += OnUpdate;
-
+Controller controller = new Controller(
+    new TelegramBotClient(botToken, cancellationToken: cts.Token),
+    idToUserState);
 
 Console.WriteLine("Для завершения работы нажмите Enter");
 
 Console.ReadLine();
-cts.Cancel(); // stop the bot
+
+// завершает работу бота
+cts.Cancel();
 
 // Сохранение состояний пользователей
 string serializedJson = JsonSerializer.Serialize<Dictionary<long, UserState>>(idToUserState);
 File.WriteAllText(USERSTATE_FILENAME, serializedJson);
-
-
-
-
-// Обработки разных ошибок
-async Task OnError(Exception exception, HandleErrorSource source)
-{
-    Console.WriteLine(exception);
-}
-
-// Обработка полученных сообщений
-async Task OnMessage(Message msg, UpdateType type)
-{
-    if (msg.Text == "/start")
-    {
-        await bot.SendMessage(msg.Chat, "Choose a response", replyMarkup: new string[][]
-        {
-            ["08:00", "08:30"],
-            ["09:00", "09:30"],
-            ["10:00", "10:30"]
-        });
-
-        //await bot.SendMessage(msg.Chat, "Выберите время для записи",
-        //    replyMarkup: buttons.ToArray());
-        //replyMarkup: new InlineKeyboardButton[] { "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00" });
-    }
-}
-
-// Обработка других обновлений
-async Task OnUpdate(Update update)
-{
-    if (update is { CallbackQuery: { } query }) // non-null CallbackQuery
-    {
-        // Удаление кастомной клавиатуры, но не инлайновой:
-        // await bot.SendMessage(chatId, "Removing keyboard", replyMarkup: new ReplyKeyboardRemove());
-        await bot.AnswerCallbackQuery(query.Id, $"Вы записались на {query.Data}");
-        await bot.SendMessage(
-            query.Message!.Chat,
-            $"Пользователь {query.From} записался на {query.Data}");
-    }
-}
-
-class UserState
-{
-    public Scenarios CurrentScenario { get; set; }
-    public List<string>? ImportantMessages { get; }
-    public bool IsAtStart { get; set; }
-
-    public UserState()
-    {
-        IsAtStart = true;
-        ImportantMessages = null;
-    }
-}
