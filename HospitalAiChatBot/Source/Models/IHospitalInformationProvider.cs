896dcd0a590af581b967bc96a53178745d8355cb
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-27 01:26:22 +0900

    Добавлен IHospitalInformationProvider

---
+++ HospitalAiChatBot/Source/Models/IHospitalInformationProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace HospitalAiChatbot.Source.Models
+{
+    /// <summary>
+    /// Предоставляет методы для доступа к информации о медицинском учреждении.
+    /// </summary>
+    public interface IHospitalInformationProvider
+    {
+        /// <summary>
+        /// Получить расписание работы в виде форматированной строки.
+        /// </summary>
+        static abstract string GetOpeningHours();
+
+
+        /// <summary>
+        /// Получить номер телефона колл-центра.
+        /// </summary>
+        static abstract string GetCallCenterContacts();
+    }
+}
