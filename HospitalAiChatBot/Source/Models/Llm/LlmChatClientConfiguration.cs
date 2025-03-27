c173dab7c04bb4628b11f55ac0e2260122769801
nemonology <nemonology@mail.ru>
2025-03-27 15:35:40 +0900

    Добавлена структура конфигурации чат-клиента с LLM

---
+++ HospitalAiChatBot/Source/Models/Llm/LlmChatClientConfiguration.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalAiChatbot.Source.Models.Llm;
+
+/// <summary>
+///     Конфигурация чта-клиента с LLM
+/// </summary>
+/// <param name="Temperature">
+///     <para>Температура выборки.</para>
+///     Чем выше значение, тем более случайным будет ответ модели
+///     <remarks>Значение по умолчанию: 0.7</remarks>
+/// </param>
+/// <param name="TopP">
+///     <para>Альтернатива <see cref="Temperature">температуре выборки.</see></para>
+///     Задает вероятностную массу токенов, которые должна учитывать модель.
+///     <remarks>Значение по умолчанию: 0.7</remarks>
+/// </param>
+/// <param name="RepetitionPenalty">
+///     <para>
+///         Величина, характеризующая повторение слов в ответе от LLM
+///     </para>
+///     Чем больше значение данной величины, тем больше модель в ответе будет стараться не повторять слова.
+///     <remarks>Нейтральное значение - 1.0</remarks>
+/// </param>
+/// <param name="MaxTokens">
+///     Максимальное количество токенов в ответе от LLM.
+///     <remarks>Значение по умолчанию: 2048</remarks>
+/// </param>
+/// <param name="IsLlmAnswerStreaming">
+///     Определяет, будет ли ответ от LLM в виде потока частей сообщения или в виде одного
+///     сообщения.
+///     <remarks>Значение по умолчанию: false</remarks>
+/// </param>
+public record LlmChatClientConfiguration(
+    float Temperature = 0.7f,
+    [Range(0, 1)] float TopP = 0.9f,
+    float RepetitionPenalty = 1.0f,
+    int MaxTokens = 2048,
+    bool IsLlmAnswerStreaming = false);
