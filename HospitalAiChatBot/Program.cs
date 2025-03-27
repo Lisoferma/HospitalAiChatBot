42f9be0a3831556cc0288255e53e18b604c2e820
aril114 <aeaw7744@gmail.com>
2025-03-27 16:47:08 +0900

    Сервер OpenAPI перемещен в проект HospitalAiChatBot, а находившийся там раньше бот телеги вынесен в отдельный проект

--- HospitalAiChatBot/Program.cs
+++ HospitalAiChatBot/Program.cs
@@ -1,54 +1,21 @@
-using HospitalAiChatbot.Source.Models;
-using System.Text.Json;
-using Telegram.Bot;
+var builder = WebApplication.CreateBuilder(args);
 
-const string TOKEN_FILENAME = "token.txt";
-const string USERSTATE_FILENAME = "userstate.json";
+// Add services to the container.
 
-string botToken;
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
-try
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    botToken = File.ReadAllText(TOKEN_FILENAME);
+    app.MapOpenApi();
 }
 
-catch (FileNotFoundException)
-{
-    Console.WriteLine("Не найден файл с токеном token.txt");
-    Console.WriteLine("Программа завершается");
-    return;
-}
+app.UseAuthorization();
 
+app.MapControllers();
 
-Dictionary<long, UserState> idToUserState;
-
-try
-{
-    // Чтение состояний пользователей из файла
-    string readenJson = File.ReadAllText(USERSTATE_FILENAME);
-    idToUserState = JsonSerializer.Deserialize<Dictionary<long, UserState>>(readenJson);
-}
-
-catch (FileNotFoundException)
-{
-    Console.WriteLine("Файл с состояниями пользователей не найден");
-    Console.WriteLine("Состояния пользователей обнуляются");
-    idToUserState = new Dictionary<long, UserState>();
-}
-
-
-using var cts = new CancellationTokenSource();
-Controller controller = new Controller(
-    new TelegramBotClient(botToken, cancellationToken: cts.Token),
-    idToUserState);
-
-Console.WriteLine("Для завершения работы нажмите Enter");
-
-Console.ReadLine();
-
-// завершает работу бота
-cts.Cancel();
-
-// Сохранение состояний пользователей
-string serializedJson = JsonSerializer.Serialize<Dictionary<long, UserState>>(idToUserState);
-File.WriteAllText(USERSTATE_FILENAME, serializedJson);
+app.Run();
---
+++ TgBot/Program.cs
@@ -0,0 +1,53 @@
+using System.Text.Json;
+using Telegram.Bot;
+
+const string TOKEN_FILENAME = "token.txt";
+const string USERSTATE_FILENAME = "userstate.json";
+
+string botToken;
+
+try
+{
+    botToken = File.ReadAllText(TOKEN_FILENAME);
+}
+
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Не найден файл с токеном token.txt");
+    Console.WriteLine("Программа завершается");
+    return;
+}
+
+
+Dictionary<long, UserState> idToUserState;
+
+try
+{
+    // Чтение состояний пользователей из файла
+    string readenJson = File.ReadAllText(USERSTATE_FILENAME);
+    idToUserState = JsonSerializer.Deserialize<Dictionary<long, UserState>>(readenJson);
+}
+
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Файл с состояниями пользователей не найден");
+    Console.WriteLine("Состояния пользователей обнуляются");
+    idToUserState = new Dictionary<long, UserState>();
+}
+
+
+using var cts = new CancellationTokenSource();
+Controller controller = new Controller(
+    new TelegramBotClient(botToken, cancellationToken: cts.Token),
+    idToUserState);
+
+Console.WriteLine("Для завершения работы нажмите Enter");
+
+Console.ReadLine();
+
+// завершает работу бота
+cts.Cancel();
+
+// Сохранение состояний пользователей
+string serializedJson = JsonSerializer.Serialize<Dictionary<long, UserState>>(idToUserState);
+File.WriteAllText(USERSTATE_FILENAME, serializedJson);
