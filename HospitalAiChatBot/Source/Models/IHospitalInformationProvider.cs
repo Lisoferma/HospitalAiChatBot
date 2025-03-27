ed550360eb9c0c9ec32289aee060295a6bdca5c2
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-28 01:23:43 +0900

    Убраны модифиакторы static. Добавлены новые методы

--- HospitalAiChatBot/Source/Models/IHospitalInformationProvider.cs
+++ HospitalAiChatBot/Source/Models/IHospitalInformationProvider.cs
@@ -8,12 +8,26 @@
         /// <summary>
         /// Получить расписание работы в виде форматированной строки.
         /// </summary>
-        static abstract string GetOpeningHours();
+        string GetOpeningHours();
 
 
         /// <summary>
         /// Получить номер телефона колл-центра.
         /// </summary>
-        static abstract string GetCallCenterContacts();
+        string GetCallCenterContacts();
+
+
+        /// <summary>
+        /// Скачать файл с ценами на медицинские услуги.
+        /// </summary>
+        /// <returns>Файл в виде масива байт.</returns>
+        Task<byte[]> DownloadPriceListAsync();
+
+
+        /// <summary>
+        /// Скачать файл с памятками для пациентов по подготовке к диагностическим исследованиям.
+        /// </summary>
+        /// <returns>Файл в виде масива байт.</returns>
+        Task<byte[]> DownloadPreparingProcedureAsync();
     }
 }
