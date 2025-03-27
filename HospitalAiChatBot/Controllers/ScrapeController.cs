7f16ce8a127813f0718eaed04d45bc08e3481bc5
aril114 <aeaw7744@gmail.com>
2025-03-27 19:43:38 +0900

    OpenAPI сервер теперь получает информацию с сайта через скрапер

---
+++ HospitalAiChatBot/Controllers/ScrapeController.cs
@@ -0,0 +1,22 @@
+﻿using HospitalAiChatbot.Source.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAiChatbot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScrapeController : ControllerBase
+    {
+        [HttpGet("openinghours")]
+        public string GetOpeningHours()
+        {
+            return ChitgmaClinicScraper.GetOpeningHours();
+        }
+
+        [HttpGet("callcentercontacts")]
+        public string GetCallCenterContacts()
+        {
+            return ChitgmaClinicScraper.GetCallCenterContacts();
+        }
+    }
+}
