83590a3c20ce9615324572c869d5b07a743a05ff
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-28 21:37:04 +0900

    Добавлен AudioFormatConverter

--- HospitalAiChatBot/Models/Services/AudioFormatConverter.cs
+++ HospitalAiChatBot/Models/Services/AudioFormatConverter.cs
@@ -1,39 +1,38 @@
 ﻿using NAudio.Wave;
 using NAudio.Vorbis;
 
-namespace HospitalAiChatbot.Models.Services
+namespace HospitalAiChatbot.Models.Services;
+
+/// <summary>
+///     Содержит методы для конвертации аудио файлов в другие форматы.
+/// </summary>
+public static class AudioFormatConverter
 {
     /// <summary>
-    /// Содержит методы для конвертации аудио файлов в другие форматы.
+    ///     Конвертирует OGG Stream в WAV byte[] с указанной частотой дискретизации, 16-bit, mono.
     /// </summary>
-    public static class AudioFormatConverter
+    /// <param name="oggStream">Входной поток OGG (должен быть читаемым)</param>
+    /// <param name="sampleRate">Частота дискретизации выходного WAV аудио.</param>
+    /// <returns>WAV данные в виде byte[]</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static byte[] ConvertOggStreamToWavBytes(
+        Stream oggStream,
+        int sampleRate = 16000,
+        int bits = 16,
+        int channels = 1)
     {
-        /// <summary>
-        /// Конвертирует OGG Stream в WAV byte[] с указанной частотой дискретизации, 16-bit, mono.
-        /// </summary>
-        /// <param name="oggStream">Входной поток OGG (должен быть читаемым)</param>
-        /// <param name="sampleRate">Частота дискретизации выходного WAV аудио.</param>
-        /// <returns>WAV данные в виде byte[]</returns>
-        /// <exception cref="ArgumentException"></exception>
-        public static byte[] ConvertOggStreamToWavBytes(
-            Stream oggStream,
-            int sampleRate = 16000,
-            int bits = 16,
-            int channels = 1)
-        {
-            if (oggStream == null || !oggStream.CanRead)
-                throw new ArgumentException("Некорректный входной поток");
+        if (oggStream == null || !oggStream.CanRead)
+            throw new ArgumentException("Некорректный входной поток");
 
-            using MemoryStream wavStream = new();
-            using VorbisWaveReader oggReader = new(oggStream);
+        using MemoryStream wavStream = new();
+        using VorbisWaveReader oggReader = new(oggStream);
 
-            using WaveFormatConversionStream resampler = new(
-                new WaveFormat(sampleRate, bits, channels),
-                oggReader);
+        using WaveFormatConversionStream resampler = new(
+            new WaveFormat(sampleRate, bits, channels),
+            oggReader);
 
-            WaveFileWriter.WriteWavFileToStream(wavStream, resampler);
+        WaveFileWriter.WriteWavFileToStream(wavStream, resampler);
 
-            return wavStream.ToArray();
-        }
+        return wavStream.ToArray();
     }
 }
