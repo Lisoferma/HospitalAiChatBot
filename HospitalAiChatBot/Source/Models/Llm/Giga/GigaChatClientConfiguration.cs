982cf6e7188912923a7bc7faf98a22313ff67cc8
nemonology <nemonology@mail.ru>
2025-03-28 16:16:03 +0900

    Добавлено поле конфигурации: модель GigaChat

--- HospitalAiChatBot/Source/Models/Llm/Giga/GigaChatClientConfiguration.cs
+++ HospitalAiChatBot/Source/Models/Llm/Giga/GigaChatClientConfiguration.cs
@@ -9,10 +9,12 @@ namespace HospitalAiChatBot.Source.Models.Llm.Giga;
 /// <param name="ApiScope">Версия API чат-клиента</param>
 /// <param name="ClientId">API ID клиента</param>
 /// <param name="ClientSecret">API secret клиента</param>
+/// <param name="Model">Используемая в чате модель</param>
 public record GigaChatClientConfiguration(
     string ClientId,
     string ClientSecret,
     GigaChatApiScope ApiScope,
+    GigaChatApiModel Model,
     float Temperature = 0.7f,
     float TopP = 0.9f,
     float RepetitionPenalty = 1,
