6bc01b5e7c51ec9d1d84408b9ea676d82e63f9e4
nemonology <nemonology@mail.ru>
2025-03-28 20:37:07 +0900

    Обновлена структура папок проекта

--- TgBot/Controller.cs
+++ TgBot/Controller.cs
@@ -1,17 +1,16 @@
 ﻿using HospitalAiChatbot.Source.Models;
-using Microsoft.AspNetCore.Http;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-class Controller
+internal class Controller
 {
-    private TelegramBotClient _bot;
-    private Dictionary<long, UserState> _idToUserState;
-    private HttpClient _httpClient;
-    private Dictionary<String, Scenarios> _strToScenario;
     private const string _webApiUrl = "http://localhost:5000";
+    private readonly TelegramBotClient _bot;
+    private readonly HttpClient _httpClient;
+    private readonly Dictionary<long, UserState> _idToUserState;
+    private readonly Dictionary<string, RequestedScenario> _strToScenario;
 
     public Controller(TelegramBotClient aBot, Dictionary<long, UserState> idToUserState)
     {
@@ -20,36 +19,36 @@ class Controller
         _bot.OnError += OnError;
         _bot.OnMessage += OnMessage;
 
-        this._idToUserState = idToUserState;
+        _idToUserState = idToUserState;
 
-        _strToScenario = new();
+        _strToScenario = new Dictionary<string, RequestedScenario>();
 
-        _strToScenario.Add("Контакты колл-центра и часы работы", Scenarios.GetWorkTimeAndContacts);
-        _strToScenario.Add("Часы работы специалиста", Scenarios.GetDoctorWorkTime);
-        _strToScenario.Add("Подготовка к исследованию", Scenarios.GetExaminationPrepareInfo);
-        _strToScenario.Add("Время изготовления анализов", Scenarios.GetSamplesPreparingTimeInfo);
-        _strToScenario.Add("Записаться к специалисту", Scenarios.MakeAppointment);
-        _strToScenario.Add("Обратная связь", Scenarios.Feedback);
-        _strToScenario.Add("Связаться с оператором", Scenarios.CommunicationWithOperator);
+        _strToScenario.Add("Контакты колл-центра и часы работы", RequestedScenario.GetWorkTimeAndContacts);
+        _strToScenario.Add("Часы работы специалиста", RequestedScenario.GetDoctorWorkTime);
+        _strToScenario.Add("Подготовка к исследованию", RequestedScenario.GetExaminationPrepareInfo);
+        _strToScenario.Add("Время изготовления анализов", RequestedScenario.GetSamplesPreparingTimeInfo);
+        _strToScenario.Add("Записаться к специалисту", RequestedScenario.MakeAppointment);
+        _strToScenario.Add("Обратная связь", RequestedScenario.Feedback);
+        _strToScenario.Add("Связаться с оператором", RequestedScenario.CommunicationWithOperator);
 
         _httpClient = new HttpClient();
     }
 
 
     /// <summary>
-    /// Обработчик для первого сообщения пользователя после /start
+    ///     Обработчик для первого сообщения пользователя после /start
     /// </summary>
     /// <param name="msg">Сообщение пользователя</param>
     /// <returns></returns>
-    async Task OnStartMessage(Message msg)
+    private async Task OnStartMessage(Message msg)
     {
-        long userId = msg.From.Id;
+        var userId = msg.From.Id;
 
         string startMsg;
 
         if (_idToUserState.ContainsKey(userId))
         {
-            UserState state = _idToUserState[userId];
+            var state = _idToUserState[userId];
             state.IsAtStart = true;
             startMsg = "Что-нибудь еще?";
         }
@@ -57,59 +56,56 @@ class Controller
         else
         {
             startMsg = "Здравствуйте! Вы обратились в чат-бот поликлиники. Мы готовы ответить на" +
-                " ваши вопросы и помочь с записью к врачу, информацией о расписании работы" +
-                " специалистов, а также предоставить данные о необходимых документах и услугах" +
-                " нашей клиники. Пожалуйста, уточните ваш вопрос или просьбу.";
-            UserState state = new UserState();
+                       " ваши вопросы и помочь с записью к врачу, информацией о расписании работы" +
+                       " специалистов, а также предоставить данные о необходимых документах и услугах" +
+                       " нашей клиники. Пожалуйста, уточните ваш вопрос или просьбу.";
+            var state = new UserState();
             _idToUserState.Add(userId, state);
         }
 
         // Добавление кнопок выполнения сценариев
-        string[] buttonNames = _strToScenario.Keys.ToArray();
+        var buttonNames = _strToScenario.Keys.ToArray();
 
-        string[][] customKeyboard = new string[_strToScenario.Count][];
+        var customKeyboard = new string[_strToScenario.Count][];
 
-        for (int i = 0; i < buttonNames.Length; i++)
-        {
-            customKeyboard[i] = new string[] { buttonNames[i] };
-        }
+        for (var i = 0; i < buttonNames.Length; i++) customKeyboard[i] = new[] { buttonNames[i] };
 
         await _bot.SendMessage(msg.Chat, startMsg, replyMarkup: customKeyboard);
     }
 
     /// <summary>
-    /// Отвечает пользователю контактами колл-центра и временем работы поликлиники
+    ///     Отвечает пользователю контактами колл-центра и временем работы поликлиники
     /// </summary>
     /// <param name="msg">Сообщение пользователя</param>
     /// <returns></returns>
-    async Task OnGetWorkTimeAndContacts(Message msg)
+    private async Task OnGetWorkTimeAndContacts(Message msg)
     {
-        HttpResponseMessage contactsResp = await _httpClient.GetAsync(_webApiUrl + "/api/scrape/callcentercontacts");
-        HttpResponseMessage workTimeResp = await _httpClient.GetAsync(_webApiUrl + "/api/scrape/openinghours");
+        var contactsResp = await _httpClient.GetAsync(_webApiUrl + "/api/scrape/callcentercontacts");
+        var workTimeResp = await _httpClient.GetAsync(_webApiUrl + "/api/scrape/openinghours");
 
-        string contacts = await contactsResp.Content.ReadAsStringAsync();
-        string workTime = await workTimeResp.Content.ReadAsStringAsync();
+        var contacts = await contactsResp.Content.ReadAsStringAsync();
+        var workTime = await workTimeResp.Content.ReadAsStringAsync();
 
         await _bot.SendMessage(msg.Chat, $"""
-            Контакты колл-центра:
-            {contacts}
+                                          Контакты колл-центра:
+                                          {contacts}
 
-            Время работы:
-            {workTime}
-            """);
+                                          Время работы:
+                                          {workTime}
+                                          """);
     }
 
 
     // Общие методы: обработчик ошибок и обработчик получений сообщений
 
     // Обработки разных ошибок
-    async Task OnError(Exception exception, HandleErrorSource source)
+    private async Task OnError(Exception exception, HandleErrorSource source)
     {
         Console.WriteLine(exception);
     }
 
     // Обработка полученных сообщений
-    async Task OnMessage(Message msg, UpdateType type)
+    private async Task OnMessage(Message msg, UpdateType type)
     {
         // Если в таблице не хранится состояния пользователя, тоже переходим к OnStartMessage
         if (msg.Text == "/start" || !_idToUserState.ContainsKey(msg.From.Id))
@@ -121,26 +117,23 @@ class Controller
 
         else if (_strToScenario.ContainsKey(msg.Text))
         {
-            Scenarios scenario = _strToScenario[msg.Text];
-            if (scenario == Scenarios.GetWorkTimeAndContacts)
-            {
-                await OnGetWorkTimeAndContacts(msg);
-            }
+            var requestedScenario = _strToScenario[msg.Text];
+            if (requestedScenario == RequestedScenario.GetWorkTimeAndContacts) await OnGetWorkTimeAndContacts(msg);
         }
     }
 }
 
 /// <summary>
-/// Состояние пользователя
+///     Состояние пользователя
 /// </summary>
-class UserState
+internal class UserState
 {
-    public Scenarios CurrentScenario { get; set; }
-    public List<string>? ImportantMessages { get; }
-    public bool IsAtStart { get; set; }
-
     public UserState()
     {
         IsAtStart = true;
     }
+
+    public RequestedScenario CurrentRequestedScenario { get; set; }
+    public List<string>? ImportantMessages { get; }
+    public bool IsAtStart { get; set; }
 }
