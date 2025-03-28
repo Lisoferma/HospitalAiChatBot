02b830557e5c2fbbc2283fa46be2d3014ff31d25
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-28 19:38:05 +0900

    Добавлен ISpeachRecognizer

---
+++ HospitalAiChatBot/Source/Models/ISpeachRecognizer.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalAiChatbot.Source.Models
+{
+    /// <summary>
+    /// Предоставляет метод для распознавания речи из аудио.
+    /// </summary>
+    public interface ISpeachRecognizer
+    {
+        /// <summary>
+        /// Распознать речь из аудио.
+        /// </summary>
+        /// <param name="audioData">Аудио файл в виде массива байт.</param>
+        /// <returns>Распознанная речь в виде текста.</returns>
+        string Recognize(byte[] audioData);
+    }
+}
