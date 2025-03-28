a7b628f0fe22b829cf29d861fef4d8ecda0a5fee
nemonology <nemonology@mail.ru>
2025-03-28 17:49:43 +0900

    Класс сообщения чата с GigaChat
    Обновлено:
    > поле `attachments` -> неизменяемые свойство;

--- HospitalAiChatBot/Source/Models/Llm/Giga/GigaChatMessage.cs
+++ HospitalAiChatBot/Source/Models/Llm/Giga/GigaChatMessage.cs
@@ -7,12 +7,12 @@ namespace HospitalAiChatBot.Source.Models.Llm.Giga;
 public class GigaChatMessage(string content, LlmChatMessageAuthorRole role, string[]? attachments = null)
     : LlmChatMessage(content, role)
 {
-    [JsonPropertyName("attachments")]
     /// <summary>
     ///     <para>Массив идентификаторов файлов, которые нужно использовать при генерации.</para>
     ///     <see cref="https://developers.sber.ru/docs/ru/gigachat/api/reference/rest/post-chat">
     ///         Подробнее о работе с файлами с GigaChat API
     ///     </see>
     /// </summary>
-    public string[] Attachments = attachments ?? [];
+    [JsonPropertyName("attachments")]
+    public string[] Attachments { get; init; } = attachments ?? [];
 }
