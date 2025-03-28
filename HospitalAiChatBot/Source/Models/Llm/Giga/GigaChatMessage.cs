699a08dfcaed30894ecae0c1a819156b15040955
nemonology <nemonology@mail.ru>
2025-03-28 16:16:52 +0900

    Добавлен класс сообщения чата GigaChat

---
+++ HospitalAiChatBot/Source/Models/Llm/Giga/GigaChatMessage.cs
@@ -0,0 +1,18 @@
+// ReSharper disable InvalidXmlDocComment
+
+using System.Text.Json.Serialization;
+
+namespace HospitalAiChatBot.Source.Models.Llm.Giga;
+
+public class GigaChatMessage(string content, LlmChatMessageAuthorRole role, string[] attachments)
+    : LlmChatMessage(content, role)
+{
+    [JsonPropertyName("attachments")]
+    /// <summary>
+    ///     <para>Массив идентификаторов файлов, которые нужно использовать при генерации.</para>
+    ///     <see cref="https://developers.sber.ru/docs/ru/gigachat/api/reference/rest/post-chat">
+    ///         Подробнее о работе с файлами с GigaChat API
+    ///     </see>
+    /// </summary>
+    public string[] Attachments = attachments;
+}
