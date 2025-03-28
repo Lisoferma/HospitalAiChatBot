ce89cb570da5e714a2718bff9a697eda289d400b
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-28 21:36:22 +0900

    Перемещён FileDownloader в папку Services

---
+++ HospitalAiChatBot/Models/Services/FileDownloader.cs
@@ -0,0 +1,23 @@
+﻿namespace HospitalAiChatBot.Models.Services;
+
+/// <summary>
+///     Содержит метод для скачивания файлов по URL.
+/// </summary>
+public static class FileDownloader
+{
+    /// <summary>
+    ///     Скачать файл по URL-адресу.
+    /// </summary>
+    /// <param name="url">URL-адрес на файл который нужно скачать.</param>
+    /// <returns>Скачанный файл в виде массива байт.</returns>
+    public static async Task<byte[]> DownloadFileAsync(string url)
+    {
+        using HttpClient httpClient = new();
+
+        var response = await httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        var fileBytes = await response.Content.ReadAsByteArrayAsync();
+
+        return fileBytes;
+    }
+}
