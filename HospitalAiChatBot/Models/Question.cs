9f12c69a1ec521de072f3aab0a0a684670a03f6f
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-30 04:46:25 +0900

    Добавлен Question

---
+++ HospitalAiChatBot/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿namespace HospitalAiChatBot.Models;
+
+public enum ClientType
+{
+    TelegramBot,
+    ChitgmaClinicSite
+}
+
+public class Question
+{
+    public ClientType FromClientType { get; set; }
+
+    public required string Contacts { get; set; }
+
+    public required string Text { get; set; }
+
+    public bool IsAnswered { get; set; } = false;
+}
