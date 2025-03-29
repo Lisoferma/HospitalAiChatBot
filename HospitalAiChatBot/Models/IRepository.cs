04b0e3440b167ae91ee2b34598399ab88c0704d5
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-30 04:46:40 +0900

    Добавлен IRepository

---
+++ HospitalAiChatBot/Models/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalAiChatBot.Models;
+
+public interface IRepository<T> where T : class
+{
+    Task<string> AddAsync(T entity);
+
+    Task<T?> GetByIdAsync(string id);
+
+    Task<IEnumerable<T?>> GetAllAsync();
+
+    Task UpdateAsync(string id, T entity);
+
+    Task DeleteAsync(string id);
+}
