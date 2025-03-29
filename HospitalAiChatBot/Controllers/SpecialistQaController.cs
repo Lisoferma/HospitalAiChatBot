5338fa901b1c2d6331ab8a3f22e9fbc77f2b5961
Lisoferma <pilipeiko.sasha@gmail.com>
2025-03-30 04:45:55 +0900

    Добавлен SpecialistQaController

---
+++ HospitalAiChatBot/Controllers/SpecialistQaController.cs
@@ -0,0 +1,35 @@
+﻿using HospitalAiChatBot.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace HospitalAiChatbot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialistQaController : ControllerBase
+    {
+        private const string CONNECTION_STRING = "mongodb://localhost:27017";
+        private const string DATABASE_NAME = "chitgmaClinic";
+        private const string COLLECTION_NAME = "questions";
+
+        private readonly IRepository<Question> _repository;
+
+
+        public SpecialistQaController()
+        {
+            _repository = new MongoRepository<Question>(CONNECTION_STRING, DATABASE_NAME, COLLECTION_NAME);
+        }
+
+
+        [HttpPost]
+        public string PostQuestion([FromBody] string text)
+        {
+            Question? question = JsonSerializer.Deserialize<Question>(text);
+
+            if (question != null)
+                _repository.AddAsync(question);
+
+            return "Благодарим вас за вопрос!";
+        }
+    }
+}
